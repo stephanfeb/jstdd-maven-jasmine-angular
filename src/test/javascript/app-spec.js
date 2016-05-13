9b8f667bdc06bb5b8df69a8da72696b8c96d250e
Pair <pair@pivotal.io>
2016-05-13 11:10:21 +0800

    initial commit

---
+++ src/test/javascript/app-spec.js
@@ -0,0 +1,31 @@
+describe('Controller: MainCtrl', function() {
+    var scope,
+        MainCtrl,
+        serviceMock;
+
+    beforeEach(module('myApp'));
+
+    serviceMock = {
+        collection: [7, 9, 42],
+        value: 42
+    };
+
+    beforeEach(inject(
+        function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+
+            MainCtrl = $controller('MainCtrl', {
+                $scope: scope,
+                myService: serviceMock
+            });
+
+            scope.$apply();
+        }
+    ));
+
+    it('mocks the service used in the controller', function() {
+        expect(MainCtrl.myObject).toEqual([7,9,42]);
+        expect(MainCtrl.myFavoriteValue).toBe(42);
+    });
+
+});
